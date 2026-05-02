0b8aefa6b07afd0a1935513f57018066759cb5c6
ElAlbany <zajdhany112@gmail.com>
2026-05-02 22:06:33 +0300

    routing

--- E-Commerce/ecommerce-project/src/App.jsx
+++ E-Commerce/ecommerce-project/src/App.jsx
@@ -1,8 +1,13 @@
+import { Routes, Route } from "react-router";
 import { HomePage } from "./Pages/HomePage";
 import "./App.css";
 
 function App() {
-  return <HomePage></HomePage>;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />}></Route>
+    </Routes>
+  );
 }
 
 export default App;
